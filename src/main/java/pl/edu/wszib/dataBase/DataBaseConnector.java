1fe9af2093f783068ab61f826ab6f7f8dad905aa
Michał <michalpodsiadlo98@gmail.com>
2019-11-17 18:50:59 +0100

    pierwszydzien

--- src/main/java/pl/edu/wszib/dataBase/DataBaseConnector.java
+++ src/main/java/pl/edu/wszib/dataBase/DataBaseConnector.java
@@ -3,5 +3,20 @@ package pl.edu.wszib.dataBase;
 import org.apache.commons.codec.digest.DigestUtils;
 import pl.edu.wszib.model.Medicine;
 
+import java.sql.*;
+
 public class DataBaseConnector {
+
+    public static Connection connection = null;
+    public static void connect(){
+        try{
+
+            Class.forName("com.mysql.jdbc.Driver");
+            DataBaseConnector.connection = DriverManager.
+                    getConnection("jdbc:mysql://localhost/wszib?user=root&password=");
+        }catch(SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
 }
